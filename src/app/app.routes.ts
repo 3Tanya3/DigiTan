3134ddda051f2c87da251fbf716c56dc619025a5
Táňa Mišinová <t.misinova@umevia.com>
2026-04-08 19:44:23 +0200

    portfolio

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import {Routes} from '@angular/router';
-import {Portfolio} from './portfolio';
-
-export const routes: Routes = [
-  { path: '', component: Portfolio },
-  { path: '**', redirectTo: '' }
-];
